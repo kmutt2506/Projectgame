4243281ac3c412c9b8fff8cba66bd60a8a64e90f
Denphum nakglam <niklaz.soulless@gmail.com>
2020-11-22 05:08:34 +0700

    incom

--- src/GamePlay/Skill.java
+++ src/GamePlay/Skill.java
@@ -1,20 +1,60 @@
 package GamePlay;
-import Player.*;
+
+import Player.Stat;
 
 public class Skill {
-    Player player1;
+
+    private Stat stat;
+    private int skillOutput = 0;
+
+    /*public Skill() {
+        stat = new Stat();
+    }*/
     
-    public Skill(){
-        String usedSkill;
-        //if(player1 == usedSkill){
-            
-        //switch(usedSkill = RandomSkill.values()[count]){
-            
-            //case "FireBall":
-        }
+    public int AttackFormPlayer1() {
+        return stat.Attack() - stat.getHp2();
+    }
+
+    public int AttackFormPlayer2() {
+        return stat.Attack() - stat.getHp1();
+    }
+    
+    
+    public int SkillOutput(){
+        Search skill = new Search();
         
-                
-                
-    }      
-//}
-//}
+        switch(skill.skill_1){
+            case "FireBall" :
+                skillOutput = stat.FireBall() - stat.getHp1();
+                break;
+            case "ThunderBolt" :
+                skillOutput = stat.ThunderBolt() - stat.getHp1();
+                break;
+            case "WindCutter" :
+                skillOutput = stat.WindCutter() - stat.getHp1();
+                break;
+            case "WaterGun" :
+                skillOutput = stat.WaterGun() - stat.getHp1();
+                break;
+            case "FrostBite" :
+                skillOutput = stat.FrostBite() - stat.getHp1();
+                break;
+            case "LifeDrain" :
+                skillOutput = stat.LifeDrain() - stat.getHp1();
+                break;
+            case "HolyLight" :
+                skillOutput = stat.HolyLight() - stat.getHp1();
+                break;
+            case "HealingTouch" :
+                skillOutput = stat.HealingTouch() + stat.getHp1();
+                break;
+            case "GreaterHeal" :
+                skillOutput = stat.GreaterHeal() + stat.GreaterHeal();
+                break;
+            case "Recovery" :
+                skillOutput = stat.Recovery() + stat.Recovery();
+                break;
+        }
+        return skillOutput;
+    }
+}
