4c50b2ff47e6403895e2d664cd147c9a6d105250
Thanakrit Paithun <Thanakrit.7653@mail.kmutt.ac.th>
2020-11-21 19:48:31 +0700

    Search ver.2

--- src/GamePlay/Search.java
+++ src/GamePlay/Search.java
@@ -18,31 +18,40 @@ public class Search {
     public String skill_2;
     public int selected;
     public  Random skill  = new Random();
-    public int count = skill.nextInt(10);
+    public int count1 = skill.nextInt(10);
+    public int count2 = skill.nextInt(10);
     public Scanner ans = new Scanner(System.in);
     public Search(){
-        this.skill_1 = RandomSkill.values()[count].toString();
+        this.skill_1 = RandomSkill.values()[count1].toString();
         
     }
     public void Activate(){
         
         if(selected == 4){
             if(skill_1 != null && skill_2 == null){
-                skill_2 = RandomSkill.values()[count].toString();
+                skill_2 = RandomSkill.values()[count2].toString();
             }else if (skill_1 == null && skill_2 != null){
-                skill_1 = RandomSkill.values()[count].toString();
+                skill_1 = RandomSkill.values()[count1].toString();
             }else if(skill_1 == null && skill_2 == null){
-                skill_1 = RandomSkill.values()[count].toString();
+                skill_1 = RandomSkill.values()[count1].toString();
             }else if(skill_1 != null && skill_2 != null){
                 System.out.println("Which skill do you want to change?(1/2)");
-                count = ans.nextInt();
-                if(count != 1 && count != 2){
+                count1 = ans.nextInt();
+                if(count1 != 1 && count1 != 2){
                     System.out.println("You lost your new skill :D");
-                }else if(count == 1){
-                    skill_1 = RandomSkill.values()[count].toString();
-                }else skill_2 = RandomSkill.values()[count].toString();                
+                }else if(count1 == 1){
+                    skill_1 = RandomSkill.values()[count1].toString();
+                }else skill_2 = RandomSkill.values()[count2].toString();                
             }
         }
 }
+    public static void main(String[] args) {
+        Search n = new Search();
+        
+        System.out.println("Skill:" + n.skill_1 + ", " + n.skill_2);
+        n.Activate();
+        System.out.println("Skill:" + n.skill_1 + ", " + n.skill_2);
+       
+    }
     
 }
