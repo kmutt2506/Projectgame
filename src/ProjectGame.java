57c919e89dab398913969098722b20121c07db31
Thanakrit Paithun <Thanakrit.7653@mail.kmutt.ac.th>
2020-11-22 01:28:44 +0700

    all change

--- src/ProjectGame.java
+++ src/ProjectGame.java
@@ -5,23 +5,53 @@ public class ProjectGame {
             
         public static void main(String[] args) {
             Scanner menu = new Scanner(System.in);
-            int ans ;
+            int ans;
+            int p1, p2;
             String name;
             Menu interf = new Menu();
-            
-            
+            Stat hp = new Stat();
+            Search skill = new Search();
                 System.out.println("*********Menu*********");
                 System.out.println("1.Play");
                 System.out.println("2.Exit");
-                
-
-           
-
+              
                 do {ans = menu.nextInt();
                     switch(ans){
-                        case 1 :System.out.print("Player 1 {Hp : " + "}\n" + interf.inMenu());
-                            
-                             System.out.println("Player 2 {Hp : }" +"}\n" + interf.inMenu());
+                        case 1 :
+                               do{
+                                System.out.print("Player 1 {Hp : " + hp.getHp1() + "}\n" + interf.inMenu());
+                                p1 = menu.nextInt();
+                                System.out.println("Player 2 {Hp : }" + hp.getHp2() +"}\n" + interf.inMenu());
+                                p2 = menu.nextInt();
+                                switch(p1){
+                                    case 1 : 
+                                         break;
+                                    case 2 : 
+                                         break;
+                                    case 3 :
+                                        break;
+                                    //case 4 : skill.Activate();
+                                        //break;
+                                }
+                                switch(p2){
+                                    case 1:
+                                    break;
+                                    case 2 : 
+                                         break;
+                                    case 3 :
+                                        break;
+                                    //case 4 : skill.Activate();
+                                        //break;
+                                }
+                                System.out.println("Result Player 1 Hp :" + hp.getHp1()+"\n" + "       " + "Player 2 Hp :" + hp.getHp2());
+                               }while(hp.getHp1() != 0 || hp.getHp2() !=0);
+                               if(hp.getHp1()==hp.getHp2()){
+                                   System.out.println("******DRAW!!!******");
+                               }
+                               else if(hp.getHp1()>hp.getHp2()){
+                                   System.out.println("Player 1 WIN!!! CONGRATS");
+                               }else System.out.println("Player 2 WIN!!! CONGRATS");
+                                
                     break;
                 }
             }while(ans != 2);
