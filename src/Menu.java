169ed58ee154a51dcff0a1767314b06c19c98f9c
Denphum nakglam <niklaz.soulless@gmail.com>
2020-11-22 05:10:12 +0700

    change

--- src/Menu.java
+++ src/Menu.java
@@ -8,9 +8,10 @@ public class Menu {
         Search skill = new Search();
         
         menu = "1.Attack\n" 
-        + "2.Skill 1 :" + skill.skill_1 +"\n" 
-        + "3.Skill 2 :" + skill.skill_2 +"\n"
-        + "4.Search for skill\n";
+        + "2.Skill 1 : " + skill.skill_1 +"\n" 
+        + "3.Skill 2 : " + skill.skill_2 +"\n"
+        + "Select : ";
+        //+ "4.Search for skill\n";
      return menu;
     }
 
