304e526bd0e9c55fcea91dfd9f6c701d4d18d372
Thanakrit Paithun <Thanakrit.7653@mail.kmutt.ac.th>
2020-11-22 01:28:18 +0700

    all change

--- src/Player/Stat.java
+++ src/Player/Stat.java
@@ -4,119 +4,69 @@ public class Stat {
 
     int hp1;
     int hp2;
+    int dmg;
+    int heal;
 
     public Stat() {
         this.hp1 = 100;
         this.hp2 = 100;
     }
-
-    public int Attack1() {
-        hp2 -= 10;
-        return hp2;
-    }
-
-    public int Attack2() {
-        hp1 -= 10;
+    public int getHp1(){
         return hp1;
     }
-
-    public int FireBall1() {
-        hp2 -= 16;
+     public int getHp2(){
         return hp2;
     }
-
-    public int FireBall2() {
-        hp1 -= 16;
-        return hp1;
+    public int Attack() {
+        dmg = 10;
+        return dmg;
+    }
+    public int FireBall() {
+        dmg = 16;
+        return dmg;
+    }
+    public int ThunderBolt() {
+       dmg = 17;
+        return dmg;
+    }
+    public int WindCutter() {
+        dmg = 8;
+        return dmg;
     }
 
-    public int ThunderBolt1() {
-        hp2 -= 17;
-        return hp2;
+    public int WaterGun() {
+        dmg = 12;
+        return dmg;
     }
 
-    public int ThunderBolt2() {
-        hp1 -= 17;
-        return hp1;
+    public int FrostBite() {
+        dmg = 15;
+        return dmg;
     }
 
-    public int WindCutter1() {
-        hp2 -= 8;
-        return hp2;
+    public int LifeDrain() {
+        dmg = 10;
+        return dmg;
     }
 
-    public int WindCutter2() {
-        hp1 -= 8;
-        return hp1;
+    public int HolyLight() {
+       dmg = 3;
+        return dmg;
     }
 
-    public int WaterGun1() {
-        hp2 -= 12;
-        return hp2;
+    public int HealingTouch() {
+        heal = 10;
+        return heal;
     }
 
-    public int WaterGun2() {
-        hp1 -= 12;
-        return hp1;
+    public int GreaterHeal() {
+        heal = 14;
+        return heal;
     }
 
-    public int FrostBite1() {
-        hp2 -= 15;
-        return hp2;
+    public int Recovery() {
+        heal = 18;
+        return heal;
     }
 
-    public int FrostBite2() {
-        hp1 -= 15;
-        return hp1;
-    }
-
-    public int LifeDrain1() {
-        hp2 -= 10;
-        return hp2;
-    }
-
-    public int LifeDrain2() {
-        hp1 -= 10;
-        return hp1;
-    }
-
-    public int HolyLight1() {
-        hp2 -= 3;
-        return hp2;
-    }
-
-    public int HolyLight2() {
-        hp1 -= 3;
-        return hp1;
-    }
-
-    public int HealingTouch1() {
-        hp1 += 10;
-        return hp1;
-    }
-
-    public int HealingTouch2() {
-        hp2 += 10;
-        return hp2;
-    }
-
-    public int GreaterHeal1() {
-        hp1 += 14;
-        return hp1;
-    }
-
-    public int GreaterHeal2() {
-        hp2 += 14;
-        return hp2;
-    }
-
-    public int Recovery1() {
-        hp1 += hp1 * 0.5;
-        return hp1;
-    }
-
-    public int Recovery2() {
-        hp2 += hp2 * 0.5;
-        return hp2;
-    }
 }
