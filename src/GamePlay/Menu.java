3ae8d08489aedc1e45adc5a1d42ea665764879f5
Hirun kongpetch <hirun.2542@mail.kmutt.ac.th>
2020-11-23 13:13:48 +0700

    beta

---
+++ src/GamePlay/Menu.java
@@ -0,0 +1,20 @@
+package GamePlay;
+
+
+import GamePlay.*;
+
+
+public class Menu {
+        String menu;
+    public String inMenu(){
+        Search skill = new Search();
+        
+        menu = "1.Attack\n" 
+        + "2.Skill 1 : " + skill.skill_1 +"\n" 
+        + "3.Skill 2 : " + skill.skill_2 +"\n"
+        + "Select : ";
+     return menu;
+    }
+
+ 
+}
