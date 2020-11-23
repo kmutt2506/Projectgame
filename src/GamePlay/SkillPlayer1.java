6b012903d30a019157caaf32b35b3018237f315e
Hirun kongpetch <hirun.2542@mail.kmutt.ac.th>
2020-11-23 17:13:25 +0700

    SkillPlay1

--- src/GamePlay/SkillPlayer1.java
+++ src/GamePlay/SkillPlayer1.java
@@ -6,8 +6,7 @@ public class SkillPlayer1 {
 
     private Stat stat = new Stat();
     private int currenthp2 = stat.getHp2();
-    private int currenthp1 = stat.getHp1();
-    private Menu ski;
+    public static RandomSkill ski = new RandomSkill();
 
     public int AttackFormPlayer1() {
         currenthp2 -= stat.Attack();
@@ -15,78 +14,96 @@ public class SkillPlayer1 {
     }
 
     public int SkillOutput1() {
-        ski = new Menu();
 
         switch (ski.skill_1) {
             case "FireBall":
                 currenthp2 -= stat.FireBall();
-
                 break;
             case "ThunderBolt":
                 currenthp2 -= stat.ThunderBolt();
-
                 break;
             case "WindCutter":
                 currenthp2 -= stat.WindCutter();
-
                 break;
             case "WaterGun":
-
                 currenthp2 -= stat.WaterGun();
-
                 break;
-
             case "FrostBite":
                 currenthp2 -= stat.FrostBite();
-
                 break;
             case "LifeDrain":
                 currenthp2 -= stat.LifeDrain();
-
                 break;
             case "HolyLight":
                 currenthp2 -= stat.HolyLight();
-
+                break;
+            case "Poison":
+                currenthp2 -= stat.Poison();
+                break;
+            case "Slap":
+                currenthp2 -= stat.Slap();
+                break;
+            case "SonicWave":
+                currenthp2 -= stat.SonicWave();
+                break;
+            case "Bite":
+                currenthp2 -= stat.Bite();
+                break;
+            case "DynamicPunch":
+                currenthp2 -= stat.DynamicPunch();
+                break;
+            case "JumpKick":
+                currenthp2 -= stat.JumpKick();
                 break;
         }
+        ski = new RandomSkill();
         return currenthp2;
     }
 
     public int SkillOutput2() {
-        ski = new Menu();
 
-        switch (ski.skill_1) {
+        switch (ski.skill_2) {
             case "FireBall":
                 currenthp2 -= stat.FireBall();
-
                 break;
             case "ThunderBolt":
                 currenthp2 -= stat.ThunderBolt();
-
                 break;
             case "WindCutter":
                 currenthp2 -= stat.WindCutter();
-
                 break;
             case "WaterGun":
-
                 currenthp2 -= stat.WaterGun();
-
                 break;
-
             case "FrostBite":
                 currenthp2 -= stat.FrostBite();
-
                 break;
             case "LifeDrain":
                 currenthp2 -= stat.LifeDrain();
-
                 break;
             case "HolyLight":
                 currenthp2 -= stat.HolyLight();
-
+                break;
+            case "Poison":
+                currenthp2 -= stat.Poison();
+                break;
+            case "Slap":
+                currenthp2 -= stat.Slap();
+                break;
+            case "SonicWave":
+                currenthp2 -= stat.SonicWave();
+                break;
+            case "Bite":
+                currenthp2 -= stat.Bite();
+                break;
+            case "DynamicPunch":
+                currenthp2 -= stat.DynamicPunch();
+                break;
+            case "JumpKick":
+                currenthp2 -= stat.JumpKick();
                 break;
         }
+        ski = new RandomSkill();
         return currenthp2;
     }
 }
